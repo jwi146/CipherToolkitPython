f0b19d49c4c1aeb8c357e399c9f5993060982cf0
James Irving <james24146@gmail.com>
2026-07-01 17:29:34 +0100

    still implementing

--- src/ciphers/modern/rsa.py
+++ src/ciphers/modern/rsa.py
@@ -3,7 +3,22 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 
 #generate public/private keys
-
+def generateKeys(privateKeyPath, publicKeyPath):
+    
+    #generate 2048-bit RSA private key
+    privateKey = rsa.generate_private_key (
+        public_exponent= 65537, #standard val
+        key_size= 2048
+    )
+    
+    #dervive public from private
+    publicKey = privateKey.public_key()
+    
+    #save the keys to a file
+    
+    #save private key
+    
+        
 
 
 #encrypt plaintext
