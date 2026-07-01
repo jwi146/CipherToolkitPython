de7857ffe668dc28df311c74febd0400b372fc2c
James Irving <james24146@gmail.com>
2026-07-01 17:17:56 +0100

    RSA started

---
+++ src/ciphers/modern/rsa.py
@@ -0,0 +1,13 @@
+#imports 
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+
+#generate public/private keys
+
+
+
+#encrypt plaintext
+
+
+
+#decrypt plaintext
